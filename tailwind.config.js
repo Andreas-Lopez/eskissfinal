b88d684505d8db166c72aeac7285abdddb6f01c0
Andreas-Lopez <andreas.lopez@edu.univ-fcomte.fr>
2022-05-13 11:17:38 +0200

    header-1

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+  extend: {
+    "colors": {
+      "Police-Footer": {
+       "1": "#ece4a0"
+      },
+      "Canson1": {
+        "1": "#f9f0e5"
+       },
+       "Canson2": {
+        "2": "#fff9f2"
+       },
+       "Boutons": {
+        "1": "#fff500"
+       }
+       
+    },
+
+    "fontFamily": {
+      header: ['Luckiest Guy']
+     },
+    
+   }
+  
+  },
+  plugins: [],
+ }
