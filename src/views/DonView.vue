4fe1bbe3ac2d21f4309b04244367a4bf9bfdb298
Andreas-Lopez <andreas.lopez@edu.univ-fcomte.fr>
2022-05-15 09:14:35 +0200

    cgv et don fini

---
+++ src/views/DonView.vue
@@ -0,0 +1,66 @@
+<template>
+  <main class="bg-Canson1-1">
+    <div class="relative text-center mb-10">
+      <img class="ml-[35%]" src="/trace.svg" alt="trace_rose" />
+      <div class="absolute top-[30%] left-[40%]">
+        <h2 class="font-Luckiest text-6xl text-black">Faire un don</h2>
+      </div>
+    </div>
+    <div class="flex">
+    <div class="ml-24">
+      <p class="font-Barlow text-black font-medium">
+        Avec Eskiss, vous avez la possiblité de participer à une oeuvre
+        caritative qui va permettre de soutenir <br />
+        financièrement et ou matériellement, les activités éducatives en faveur
+        des enfants. Cette association est <br />
+        Sourire d’enfant. L’association a été créée en 2012 et est régie par la
+        loi 1901 à vocation sociale. <br />
+      </p>
+      <p class="font-Barlow text-black font-medium pt-10">
+        Elle s’engage à soutenir financièrement en trois axes : <nav class=" font-Barlow text-black font-medium"><br />
+        Les hôpitaux / services de pédiatrie <br />
+        Les familles concernées <br />
+        Les associations locales <br /></nav> 
+      </p>
+      <p class="font-Barlow text-black font-medium pt-10">
+        Parce qu’il n’y a rien de plus beau que le sourire d’un enfant, auquel
+        les commerçants et les professionnels <br />
+        de santé contribuent grâce à leur générosité.
+      </p>
+      <p class="font-Barlow text-black font-medium">
+        Pour plus d’informations, rendez-vous sur le site officiel de Sourire
+        d’enfant.
+      </p>
+
+      <button
+        class="
+          bg-Boutons-1
+          hover:bg-black
+          text-black
+          py-2
+          px-7
+          hover:border-black hover:text-Boutons-1
+          rounded-3xl
+          border-2 border-black
+          font-Barlow font-bold
+          text-xl
+          content-center
+          items-stretch
+          ml-32
+          mt-10
+          mb-10
+        "
+      >
+        Faire un don
+      </button>
+    </div>
+<div>
+    <img
+      class="flex ml-24 pb-10 w-96"
+      src="/sourire.png"
+      alt="logo sourire d'enfant"
+    />
+    </div>
+    </div>
+  </main>
+</template>
